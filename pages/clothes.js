5b83151009c1f43c64c2376437f10f7b8a6f057b
Shumpei <shumpei.o.7g@gmail.com>
2022-05-25 02:43:13 +0900

    Update ファッションページ実装

--- pages/clothes.js
+++ pages/clothes.js
@@ -35,28 +35,83 @@ export default function Clothes() {
 
                     <div className="mb-60">
                         <h3>SALONE</h3>
-                        <p>ぼたんといぶきで初めてのおでかけで訪れたセレクトショップ(店名は登場しなかった)。作中では代官山にあるが、モデルとなったのは千葉・柏にあるSALONE(サローネ)というセレクトショップ。</p>
+                        <p>ぼたんといぶきで初めてのおでかけで訪れたセレクトショップ(店名は登場しなかった)。</p>
+                        <p>作中では代官山にあるが、モデルとなったのは千葉・柏にあるSALONE(サローネ)というセレクトショップ。</p>
+                        <p>お店の窓にはずらりとお酒のボトルが飾られているのがポイント。<br />アパレルと飲食できるフードスタンドが併設されている形態のお店です。</p>
                         <div className="mt-16 mb-16">
                             <Image width={400} height={600} objectFit="contain" src="/assets/img/comic_salone.jpg" alt="SALONEがモデルとなった店が登場するページ" />
-                            <p className="inyouText">引用：「{master.title}」第1巻 p53</p>
+                            <p className="inyouText">引用：<a className="underline" href="https://amzn.to/39SUayc" target='_blank' rel='noopener noreferrer'>「{master.title}」第1巻 p53</a></p>
                         </div>
                         <div className="mt-16 mb-16">
-                            <p className="mb-16">モデルとなったショップはこちら。</p>
+                            <p className="mb-16">モデルとなったSALONEさんの外観。</p>
                             <Image width={400} height={300} objectFit="contain" src="https://gigaplus.makeshop.jp/salone/icon/shop.jpg" alt="SALONEの外観" />
                             <Image width={400} height={240} objectFit="contain" src="https://gigaplus.makeshop.jp/salone/img/NEWTOP.jpg" alt="SALONEの外観" />
                             <p className="inyouText mb-16">引用：<a className="underline" href="http://salone-online.net/html/company.html" target="_blank" rel="noopener noreferrer">SALONE公式サイト</a></p>
-                            <p>SALONEは「<span className="bold">AURALEE</span>」、「<span className="bold">BALENCIAGA</span>」、「<span className="bold">MAISON MARGIELA</span>」などの有名ブランドを扱うほか、「<span className="bold">EYEVAN</span>」や「<span className="bold">MYKITA</span>」などのサングラスブランドもラインナップとしてあるのが特徴。</p>
-                            <div className="mt-32">
-                                <p>AURALEE</p>
+
+                            <div className="mt-32 mb-60">
+                                <div className={style.info}>
+                                    <div className="bold mb-4">SALONE</div>
+                                    <div>〒277-0852 千葉県柏市旭町1-4-7 タクビル1F</div>
+                                    <div>TEL 04-7147-9959</div>
+                                    <div>営業時間：アパレル12:00-19:00</div>
+                                    <div>営業時間：カフェ・フードスタンド12:00スタート</div>
+                                    <div className="ml-60">(ベルギービール・ウィスキー・ワイン・おまかせ料理)</div>
+                                    <div>定休日：不定休</div>
+                                    <div>HP：<a className="underline" href="http://salone-online.net/" target="_blank" rel="noopener noreferrer">http://salone-online.net/</a></div>
+                                </div>
                             </div>
+
+                            <p>SALONEは「<span className="bold">AURALEE(オーラリー)</span>」、「<span className="bold">BALENCIAGA(バレンシアガ)</span>」、「<span className="bold">MAISON MARGIELA(メゾン・マルジェラ)</span>」などの有名ブランドを扱うほか、「<span className="bold">EYEVAN(アイヴァン)</span>」や「<span className="bold">MYKITA(マイキータ)</span>」などのサングラスブランドもラインナップとしてあるのが特徴です。</p>
+
                             <div className="mt-32">
-                                <p>BALENCIAGA</p>
+                                <p className="bold">MAISON MARGIELA</p>
+                                <p className="mt-16">メゾン・マルジェラは1988年にデザイナーのマルタン・マルジェラによって設立されたフランスの高級ブランド。オートクチュール風と既製服、2つのコレクションを発表しています。</p>
+                                <p className="mt-16">手がけるプロダクトとしては、レディースウェア、メンズウェア、ジュエリー、シューズ、オブジェ、フレグランス、ホームグッズなど多岐にわたっています。</p>
+                                <p className="mt-16">デザインの独創性はもちろんのこと、地下鉄の駅や街角などでライブショーを行ったり、デザインを引き立たせるためにモデルの顔を布で覆うなど、型破りな表現方法を行うのも特徴です。</p>
+                                <p>ファッション業界への影響力も強く、歴史に名を残すメゾンの1つと言われています。</p>
+                                <p className="mt-16">設立当初のブランド名は「メゾン・マルタン・マルジェラ」だったが、2015年に現在の「メゾン・マルジェラ」に解明されました。</p>
+                                <p className="mt-16">現在マルジェラ本人はクリエイティブ・デザイナーを退き、後任のジョン・ガリアーノが務めています。</p>
+                                <div>
+                                    <div className="mt-32 inline-block">
+                                        <div dangerouslySetInnerHTML={{__html: getSanitizeHtml(clothesItems.margiela.tabi.url) }}></div>
+                                        <a href={clothesItems.margiela.tabi.shortUrl} className="underline bold text_ms" target='_blank' rel='noopener noreferrer'>Rakutenで見る</a>
+                                    </div>
+
+                                    <div className="mt-32 inline-block">                                
+                                        <div dangerouslySetInnerHTML={{__html: getSanitizeHtml(clothesItems.margiela.packT.url) }}></div>
+                                        <a href={clothesItems.margiela.packT.shortUrl} className="underline bold text_ms" target='_blank' rel='noopener noreferrer'>Rakutenで見る</a>
+                                    </div>
+                                </div>
+
+                                <div className={`${style.info} mt-32 mb-60`}>
+                                    <p className="bold mb-4">MAISON MARGIELA</p>
+                                    <a className="underline" href="https://www.maisonmargiela.com/" target="_blank" rel="noopener noreferrer">https://www.maisonmargiela.com/</a>
+                                </div>
                             </div>
+
                             <div className="mt-32">
-                                <p>MAISON MARGIELA</p>
-                                {/* マルジェラの説明やリンクを載せる */}
-                                <div dangerouslySetInnerHTML={{__html: getSanitizeHtml(clothesItems.tabi.url) }}></div>
-                                <a href="https://a.r10.to/hwKExP" className="underline bold text_ms" target='_blank' rel='noopener noreferrer'>Rakutenで見る</a>
+                                <p className="bold">MYKITA</p>
+                                <p className="mt-16">マイキータはドイツのアイウェアブランドです。特許を取得した画期的なアイデアや確かな技術力・特徴的なデザインと様々なコラボアイテムが存在しているのが特徴です。</p>
+                                <p className="mt-16">デザイン〜製造まですべてベルリンにある自社工房で行われており、最新技術を使ったハイテク加工と、手作業による丁寧な仕事によって作られています。</p>
+                                <p>MYKITAというブランド名は、ドイツ語で保育施設という意味の「KITA」と、英語の「MY」を組み合わせた造語です。これは、最初のオフィスが元々保育園であったことが由来となっています。</p>
+                                <p>ちなみに、ドイツは日本のように保育園・幼稚園、託児所などの区別はあまりなく、保育施設を総称してKita（Kindertagesstätte）と呼んでいます。</p>
+                                {/* MYKITAの説明やリンクを載せる */}
+                                <div>
+                                    <div className="mt-32 inline-block">
+                                        <div dangerouslySetInnerHTML={{__html: getSanitizeHtml(clothesItems.mykita.eldermh1.url) }}></div>
+                                        <a href={clothesItems.mykita.eldermh1.shortUrl} className="underline bold text_ms" target='_blank' rel='noopener noreferrer'>Rakutenで見る</a>
+                                    </div>
+
+                                    <div className="mt-32 inline-block">
+                                        <div dangerouslySetInnerHTML={{__html: getSanitizeHtml(clothesItems.mykita.margielaCollabo.url) }}></div>
+                                        <a href={clothesItems.mykita.margielaCollabo.shortUrl} className="underline bold text_ms" target='_blank' rel='noopener noreferrer'>Rakutenで見る</a>
+                                    </div>
+
+                                    <div className={`${style.info} mt-32 mb-60`}>
+                                        <p className="bold mb-4">MYKITA</p>
+                                        <a className="underline" href="https://mykita.com/" target="_blank" rel="noopener noreferrer">https://mykita.com/</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
--- storage/clothes.js
+++ storage/clothes.js
@@ -1,7 +1,24 @@
 const clothesItems = {
-    tabi: {
-        url: `<a href="https://hb.afl.rakuten.co.jp/ichiba/27ca17ec.07c68cc7.27ca17ed.d0efe416/_RTLink45436?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fimportshopdouble%2F29147%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MSwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hgb/27ca17ec.07c68cc7.27ca17ed.d0efe416/?me_id=1233879&item_id=10038570&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fimportshopdouble%2Fcabinet%2Fmartinmargiela%2F29147_0.jpg%3F_ex%3D240x240&s=240x240&t=pict" border="0" style="margin:2px" alt="" title=""></a>`
+    margiela: {
+        tabi: {
+            url: `<a href="https://hb.afl.rakuten.co.jp/ichiba/27ca17ec.07c68cc7.27ca17ed.d0efe416/_RTLink45436?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fimportshopdouble%2F29147%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJyaWdodCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MSwiYm9yIjoxLCJjb2wiOjEsImJidG4iOjEsInByb2QiOjAsImFtcCI6ZmFsc2V9" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hgb/27ca17ec.07c68cc7.27ca17ed.d0efe416/?me_id=1233879&item_id=10038570&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fimportshopdouble%2Fcabinet%2Fmartinmargiela%2F29147_0.jpg%3F_ex%3D240x240&s=240x240&t=pict" border="0" style="margin:2px" alt="" title=""></a>`,
+            shortUrl: "https://a.r10.to/hwKExP"
+        },
+        packT: {
+            url: `<a href="https://hb.afl.rakuten.co.jp/ichiba/27f01fee.3faa4ed8.27f01fef.8b9ef6c8/_RTLink45436?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fonesanagehabm%2Fs52gc0189s23901100%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJkb3duIiwiY29tIjoxLCJjb21wIjoiZG93biIsInByaWNlIjowLCJib3IiOjAsImNvbCI6MSwiYmJ0biI6MCwicHJvZCI6MCwiYW1wIjpmYWxzZX0%3D" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hgb/27f01fee.3faa4ed8.27f01fef.8b9ef6c8/?me_id=1308102&item_id=10034645&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fonesanagehabm%2Fcabinet%2Fmm6%2Fmm6%2Fs52gc0189s23901100.jpg%3F_ex%3D240x240&s=240x240&t=pict" border="0" style="margin:2px" alt="" title=""></a>`,
+            shortUrl: "https://a.r10.to/h6VEja"
+        }
     },
+    mykita: {
+        eldermh1: {
+            url: `<a href="https://hb.afl.rakuten.co.jp/ichiba/27f02f6b.a8e04b66.27f02f6c.3af9600a/_RTLink45436?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Fcourage-store%2F337c0792facb13c-mc%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJ1cCIsImNvbSI6MSwiY29tcCI6ImRvd24iLCJwcmljZSI6MCwiYm9yIjowLCJjb2wiOjEsImJidG4iOjAsInByb2QiOjAsImFtcCI6ZmFsc2V9" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hgb/27f02f6b.a8e04b66.27f02f6c.3af9600a/?me_id=1304511&item_id=11738131&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Fcourage-store%2Fcabinet%2Fmc%2F32%2F337c0792facb13c-mc-2.jpg%3F_ex%3D240x240&s=240x240&t=pict" border="0" style="margin:2px" alt="" title=""></a>`,
+            shortUrl: "https://a.r10.to/haPeqP"
+        },
+        margielaCollabo: {
+            url: `<a href="https://hb.afl.rakuten.co.jp/ichiba/27f02fc9.1868ffc7.27f02fca.30d1b098/_RTLink45436?pc=https%3A%2F%2Fitem.rakuten.co.jp%2Ffermart%2Fhc-221168m133008%2F&link_type=pict&ut=eyJwYWdlIjoiaXRlbSIsInR5cGUiOiJwaWN0Iiwic2l6ZSI6IjI0MHgyNDAiLCJuYW0iOjEsIm5hbXAiOiJkb3duIiwiY29tIjoxLCJjb21wIjoiZG93biIsInByaWNlIjowLCJib3IiOjAsImNvbCI6MSwiYmJ0biI6MCwicHJvZCI6MCwiYW1wIjpmYWxzZX0%3D" target="_blank" rel="nofollow sponsored noopener" style="word-wrap:break-word;"  ><img src="https://hbb.afl.rakuten.co.jp/hgb/27f02fc9.1868ffc7.27f02fca.30d1b098/?me_id=1301832&item_id=28700079&pc=https%3A%2F%2Fthumbnail.image.rakuten.co.jp%2F%400_mall%2Ffermart%2Fcabinet%2Fhcimg812%2Fhc-221168m133008.jpg%3F_ex%3D240x240&s=240x240&t=pict" border="0" style="margin:2px" alt="" title=""></a>`,
+            shortUrl: "https://a.r10.to/hMkqQJ"
+        }
+    }
 };
 
 export default clothesItems;
