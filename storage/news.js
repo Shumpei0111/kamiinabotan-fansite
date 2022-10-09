ffad90847fa3f8ee4672fffdea63f3d110faf75e
Shumpei <shumpei.o.7g@gmail.com>
2022-10-09 21:21:58 +0900

    Update ニュース更新

--- storage/news.js
+++ storage/news.js
@@ -3,15 +3,58 @@ const tags = {
     goods: "グッズ",
     update: "アップデート",
     campaign: "キャンペーン",
+    other: "その他",
 };
 
 
 const news = [
     {
+        date: "2022.10.06",
+        head: "PIXIV FANBOXで「【上伊那ぼたん】5話のお酒などを振り返る」がアップされました",
+        url: "https://tonarinohey.fanbox.cc/posts/4556544",
+        tag: tags.goods
+    },
+    {
+        date: "2022.10.04",
+        head: "PIXIV FANBOXで「【上伊那ぼたん】4話のお酒などを振り返る」がアップされました",
+        url: "https://tonarinohey.fanbox.cc/posts/4542199",
+        tag: tags.goods
+    },
+    {
+        date: "2022.10.04",
+        head: "PIXIV FANBOXで「【上伊那ぼたん】単行本1巻カバー案ラフ」がアップされました",
+        url: "https://tonarinohey.fanbox.cc/posts/4547698",
+        tag: tags.goods
+    },
+    {
+        date: "2022.10.01",
+        head: "PIXIV FANBOXで「【上伊那ぼたん】3話のお酒を振り返る」がアップされました",
+        url: "https://tonarinohey.fanbox.cc/posts/4527795",
+        tag: tags.goods
+    },
+    {
+        date: "2022.09.29",
+        head: "PIXIV FANBOXで「【上伊那ぼたん】2話のお酒を振り返る」がアップされました",
+        url: "https://tonarinohey.fanbox.cc/posts/4508400",
+        tag: tags.goods
+    },
+    {
+        date: "2022.09.27",
+        head: "PIXIV FANBOXで「【上伊那ぼたん】1話のお酒を振り返る」がアップされました",
+        url: "https://tonarinohey.fanbox.cc/posts/4504510",
+        tag: tags.goods
+    },
+    {
         date: "2022.09.27",
         head: "第33話が更新されました！",
         url: "https://mangacross.jp/comics/kamiinabotan/36",
         tag: tags.episode,
+    },
+    {
+        date: "2022.09.20",
+        head: "作者の塀先生がPIXIV FANBOXをスタートしました！",
+        url: "https://tonarinohey.fanbox.cc/posts/4472682?utm_campaign=post_page&utm_medium=share&utm_source=twitter",
+        tag: tags.goods
     },
     {
         date: "2022.08.30",
