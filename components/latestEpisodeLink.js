b2c8a5de060eb2ff5a9baffa6b15f35707886757
Shumpei <shumpei.o.7g@gmail.com>
2022-11-27 03:34:36 +0900

    ニュースが最新でなかったのを修正

--- components/latestEpisodeLink.js
+++ components/latestEpisodeLink.js
@@ -1,13 +1,21 @@
 export default function LatestEpisodeLink() {
-    const episode_num = '33';
-    const episode_link = 'https://mangacross.jp/comics/kamiinabotan/36';
-    const next_update = '10月25日(火)';
+    const current_episode_num = '34';
+    const current_episode_link = 'https://mangacross.jp/comics/kamiinabotan/37';
+    const next_update_date = '11月29日(火)';
 
     return (
         <div>
-            <p className="bold">最新{`${episode_num}`}話公開中！</p>
-            <a className="underline" href={`${episode_link}`} target="_blank" rel="noreferrer noopener">{`${episode_link}`}</a>
-            <p className="mt-16">次回更新日&nbsp;👉&nbsp;&nbsp;&nbsp;<span className="bold">{`${next_update}`}</span></p>
+            <p className="bold">最新{`${current_episode_num}`}話公開中！</p>
+            <a
+                className="underline"
+                href={`${current_episode_link}`}
+                target="_blank"
+                rel="noreferrer noopener"
+            >{`${current_episode_link}`}</a>
+            <p className="mt-16">
+                次回更新日&nbsp;👉&nbsp;&nbsp;&nbsp;
+                <span className="bold">{`${next_update_date}`}</span>
+            </p>
         </div>
-    )
+    );
 }
