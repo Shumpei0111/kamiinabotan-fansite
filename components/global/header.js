9b1cb4f0bdc69b81e5319b8de4fbe063a6ef0d60
Shumpei <shumpei.o.7g@gmail.com>
2022-12-02 02:27:31 +0900

    半額セールが終わったのでヘッダーのマーキーの文を修正

--- components/global/header.js
+++ components/global/header.js
@@ -11,28 +11,34 @@ export default function Header(props) {
     const scroll = useScrollHandler();
     const hasAlert = true;
 
-    const [ narrowViewClassName, setNarrowViewClassName ] = useState( "" );
+    const [narrowViewClassName, setNarrowViewClassName] = useState('');
     useEffect(() => {
-        if(scroll) {
-            setNarrowViewClassName("narrowView");
+        if (scroll) {
+            setNarrowViewClassName('narrowView');
         } else {
-            setNarrowViewClassName("");
+            setNarrowViewClassName('');
         }
     }, [scroll]);
 
     return (
         <header id="gHeader" className={style.header}>
-            {
-                hasAlert ? (
-                    <div className={style.alert}>
-                        <Marquee speed={50} gradient={false}>
-                            {/* <a href="https://amzn.to/37VR9MD" className={`underline ${style.marqueeText}`} target="_blank" rel="noopener noreferrer">「{master.title}」第３巻好評発売中！</a> */}
-                            <a href="https://amzn.to/3SgDimN" className={`underline ${style.marqueeText}`} target="_blank" rel="noopener noreferrer">Kindle版第1巻が半額セール中！</a>
-                        </Marquee>
-                    </div>
-                ) :
+            {hasAlert ? (
+                <div className={style.alert}>
+                    <Marquee speed={50} gradient={false}>
+                        <a
+                            href="https://amzn.to/37VR9MD"
+                            className={`underline ${style.marqueeText}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            「{master.title}」第３巻好評発売中！
+                        </a>
+                        {/* <a href="https://amzn.to/3SgDimN" className={`underline ${style.marqueeText}`} target="_blank" rel="noopener noreferrer">Kindle版第1巻が半額セール中！</a> */}
+                    </Marquee>
+                </div>
+            ) : (
                 <></>
-            }
+            )}
             <div className={`${style.siteTitleWrapper} ${narrowViewClassName}`}>
                 <Link href="/">
                     <a className="relative">
@@ -45,5 +51,5 @@ export default function Header(props) {
                 </Link>
             </div>
         </header>
-    )
+    );
 }
