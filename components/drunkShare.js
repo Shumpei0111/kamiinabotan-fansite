686e841c3018074df2ca16f222d4a627fee12258
Shumpei <shumpei.o.7g@gmail.com>
2022-12-10 03:22:42 +0900

    update スタイルの調整

--- components/drunkShare.js
+++ components/drunkShare.js
@@ -1,5 +1,6 @@
 import { TwitterShareButton, TwitterIcon } from 'react-share';
 import { SITE_FULL_TITLE } from '../lib/constraint';
+import * as style from '../styles/drunkShare.module.scss';
 
 export const DrunkShare = ({ liq }) => {
     const liqourName = liq.name;
@@ -8,9 +9,14 @@ export const DrunkShare = ({ liq }) => {
     const pageURL = `https://yuriyoi.site/liquorlist#${liq.id}`;
 
     return (
-        <div className="frame">
-            <TwitterShareButton url={pageURL} title={sharePageTitle} via="seventhseven">
-                <div className="flex align-item-center">
+        <div className={`frame ${style.shareFrame}`}>
+            <TwitterShareButton
+                className={style.shareButton}
+                url={pageURL}
+                title={sharePageTitle}
+                via="seventhseven"
+            >
+                <div className={`flex align-item-center justify-center ${style.shareWrapper}`}>
                     <TwitterIcon size={30} round={true} />
                     <span>飲んだらシェアしよう！</span>
                 </div>
