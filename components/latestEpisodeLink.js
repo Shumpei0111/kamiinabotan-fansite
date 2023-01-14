ec923cc1819a843a527195621ef1f8713aa07bb7
Shumpei <shumpei.o.7g@gmail.com>
2023-01-14 12:06:16 +0900

    Update 次回更新日の日付更新

--- components/latestEpisodeLink.js
+++ components/latestEpisodeLink.js
@@ -1,7 +1,7 @@
 export default function LatestEpisodeLink() {
     const current_episode_num = '35';
     const current_episode_link = 'https://mangacross.jp/comics/kamiinabotan/38';
-    const next_update_date = '12月27日(火)';
+    const next_update_date = '1月31日(火)';
 
     return (
         <div>
