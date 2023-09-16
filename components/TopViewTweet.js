d245e22e3baca74dab94358561c22728af5eb5fb
Shumpei <shumpei.o.7g@gmail.com>
2023-09-16 09:01:21 +0900

    fix typo

--- components/TopViewTweet.js
+++ components/TopViewTweet.js
@@ -1,7 +1,7 @@
 export default function TopViewTweet() {
     return (
         <div>
-            <blockquote class="twitter-tweet">
+            <blockquote className="twitter-tweet">
                 <p lang="ja" dir="ltr">「上伊那ぼたん、酔へる姿は百合の花」とコラボアイテムは絶賛受注販売中！！<br/><br/>第一弾はオリジナルイラストTシャツ<br/>ご注文は下記サイトから↓<br/>
                 <a href="https://t.co/s6UzPVwqum">https://t.co/s6UzPVwqum</a><br/><br/>
                 第二弾も鋭意制作中〜🍶😁<br/>Tシャツ買って待っててくださいね〜！ <a href="https://t.co/dRYdry2WAd">pic.twitter.com/dRYdry2WAd</a></p>&mdash; 地産地消Cafe&amp;Bar Clusterと酒屋Cluster Bottle Shopの中の人 (@Cluster54761099) <a href="https://twitter.com/Cluster54761099/status/1702140377929843071?ref_src=twsrc%5Etfw">September 14, 2023</a></blockquote> 
