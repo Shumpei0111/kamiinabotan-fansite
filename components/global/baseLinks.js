d1927cfcc7eea493b33b243fc9462a5d75a7a932
Shumpei <shumpei.o.7g@gmail.com>
2023-09-30 02:15:16 +0900

    feat: デザイン調整

--- components/global/baseLinks.js
+++ components/global/baseLinks.js
@@ -7,23 +7,27 @@ export default function BaseLinks() {
     return (
         <div className={style.baseLinksContaienr}>
             <ul className={style.baseLinksList}>
-                <li>
+                <li className="relative">
+                    <span className='head__en' role="presentation">Chara</span>
                     <Link href="/characters">
                         <a>キャラクターを見る</a>
                     </Link>
                 </li>
-                <li>
-                    <Link href="/liquorlist">
+                <li className="relative">
+                    <span className='head__en' role="presentation">Liqour</span>
+                    <Link className="relative" href="/liquorlist">
                         <a>お酒一覧を見る</a>
                     </Link>
                 </li>
-                <li>
-                    <Link href="/quiz">
+                <li className="relative">
+                    <span className='head__en' role="presentation">Quiz</span>
+                    <Link className="relative" href="/quiz">
                         <a>上伊那ぼたんクイズ</a>
                     </Link>
                 </li>
-                <li>
-                    <Link href="/clothes">
+                <li className="relative">
+                    <span className='head__en' role="presentation">Fashion</span>
+                    <Link className="relative" href="/clothes">
                         <a>ファッションで見る</a>
                     </Link>
                 </li>
